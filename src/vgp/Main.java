8036a6f746b9c742c93ae8db120890011efdb630
Joakim Sandström <kongzhu@hotmail.com>
2022-06-17 17:33:07 +0200

    Material db
    Material database added. Will not be part of the final version.

--- src/vgp/Main.java
+++ src/vgp/Main.java
@@ -18,34 +18,34 @@ public class Main {
     public static void main(String[] args) throws IOException{
         long startTime = System.nanoTime();
         IO io = new IO();
-        Material matl = new Material();
-        FEM fem = new FEM();
-        Constants cons = new Constants();
+        Material m = new Material();
+        FEM f = new FEM();
+        Constants c = new Constants();
         io.InputOutput();
-        cons.setConstants(io.configList);
+        c.setConstants(io.configList);
 
 //Explicit solver, Specific heat, each layer is one element
         if(Constants.MODEL == 1){
             System.out.println("Model 1");
-            matl.material(io.inputList, false);
-            fem.fem(io.getTime(), matl.getLayerList(), cons.getAdiabatisk());
-            io.SystemOut(fem.getTarray());
+            m.material(io.inputList, false);
+            f.fem(io.getTime(), m.getLayerList(), c.getAdiabatisk());
+            io.SystemOut(f.getTarray());
         }
 
 //Explicit solver, Specific heat, each layer is divided into multiple elements - Default model
         else if(Constants.MODEL == 2){
             System.out.println("Model 2");
-            matl.material(io.inputList, true);
-            fem.femSplit(io.getTime(), matl.getLayerListSplit(), cons.getAdiabatisk());
-            io.SystemOutSplit(matl.getLayerThickness(), fem.getTarray(), matl.getLayerCount());
+            m.material(io.inputList, true);
+            f.femSplit(io.getTime(), m.getLayerListSplit(), c.getAdiabatisk());
+            io.SystemOutSplit(m.getLayerThickness(), f.getTarray(), m.getLayerCount());
         }
 
 //Explicit solver, Enthalpy, each layer is divided into multiple elements
         else if(Constants.MODEL == 3){
             System.out.println("Model 3");
-            matl.material(io.inputList, true);
-            fem.femSplit(io.getTime(), matl.getLayerListSplit(), cons.getAdiabatisk());
-            io.SystemOut(fem.getTarray());
+            m.material(io.inputList, true);
+            f.femSplit(io.getTime(), m.getLayerListSplit(), c.getAdiabatisk());
+            io.SystemOut(f.getTarray());
         }
 
         long endTime = System.nanoTime();
